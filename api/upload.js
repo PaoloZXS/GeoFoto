ee2068d57ea747b2abf87efffcdf4feffe6c68fa
Paolo Giorsetti <paolo.giorsetti@codarini.local>
2026-07-07 09:22:55 +0200

    fix

--- api/upload.js
+++ api/upload.js
@@ -1,21 +1,11 @@
-import formidable from 'formidable';
-import fs from 'fs';
-import ftp from 'basic-ftp';
-import path from 'path';
+const formidable = require('formidable');
+const fs = require('fs');
+const { Client } = require('basic-ftp');
 
-export const config = { api: { bodyParser: false } };
-
-const FTP_HOST = process.env.FTP_HOST || '62.110.25.18';
-const FTP_PORT = parseInt(process.env.FTP_PORT || '21');
-const FTP_USER = process.env.FTP_USER || 'CondivisioneFoto';
-const FTP_PASS = process.env.FTP_PASS || '3621spectrum5152';
-const FTP_ROOT = 'FotoLavori';
-
-export default async function handler(req, res) {
+async function handler(req, res) {
     if (req.method !== 'POST') return res.status(405).send('Method not allowed');
 
     try {
-        // Leggi il form data
         const form = formidable({});
         const [fields, files] = await form.parse(req);
         const cliente = fields.cliente?.[0] || 'Sconosciuto';
@@ -23,11 +13,16 @@ export default async function handler(req, res) {
 
         if (!file) return res.status(400).send('Nessun file ricevuto');
 
+        const FTP_HOST = process.env.FTP_HOST || '62.110.25.18';
+        const FTP_PORT = parseInt(process.env.FTP_PORT || '21');
+        const FTP_USER = process.env.FTP_USER || 'CondivisioneFoto';
+        const FTP_PASS = process.env.FTP_PASS || '3621spectrum5152';
+        const FTP_ROOT = 'FotoLavori';
+
         const remoteDir = `${FTP_ROOT}/${cliente}`;
         const remotePath = `${remoteDir}/${file.originalFilename}`;
 
-        // Upload FTP
-        const client = new ftp.Client();
+        const client = new Client();
         client.ftp.verbose = false;
 
         await client.access({
@@ -42,7 +37,6 @@ export default async function handler(req, res) {
         await client.uploadFrom(file.filepath, remotePath);
         client.close();
 
-        // Pulisci file temporaneo
         fs.unlink(file.filepath, () => {});
 
         res.status(200).send('OK');
@@ -51,3 +45,6 @@ export default async function handler(req, res) {
         res.status(500).send(e.message);
     }
 }
+
+module.exports = handler;
+module.exports.config = { api: { bodyParser: false } };
